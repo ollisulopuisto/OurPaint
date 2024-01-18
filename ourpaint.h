1ced06428c6312dcee82fc6c60bbfee05e319d77
YimingWu <xp8110@outlook.com>
2024-01-18 21:32:36 +0800

    Version 0.2.1

--- ourpaint.h
+++ ourpaint.h
@@ -51,7 +51,7 @@ extern const char OUR_COMPOSITION_SHADER[];
 
 #define OUR_VERSION_MAJOR 0
 #define OUR_VERSION_MINOR 2
-#define OUR_VERSION_SUB 0
+#define OUR_VERSION_SUB 1
 
 #define OUR_PAINT_NAME_STRING "Our Paint v0.2"
 
